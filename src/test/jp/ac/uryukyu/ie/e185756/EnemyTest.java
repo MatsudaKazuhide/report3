23a365178faea2f8ff798a60466d8746c10be18e
Kazuhide Matsuda <e185756@ie.u-ryukyu.ac.jp>
2018-11-12 00:02:26 +0900

    ステップ1 commit

--- src/test/jp/ac/uryukyu/ie/e185756/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e185756/EnemyTest.java
@@ -12,8 +12,10 @@ class EnemyTest {
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
         enemy.dead = true;
-        for(int i=0; i<10; i++) {
-            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+        if(enemy.dead == false) {
+            for (int i = 0; i < 10; i++) {
+                enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+            }
         }
         assertEquals(heroHP, hero.hitPoint);
     }
